61a627fdfdcf89b6d4c946018f95a7b70d18f3fd
dvioletta <anita@anitasquires.com>
2019-04-02 09:48:14 +0100

    Cleaning up stuff that is not required after review

--- src/main/java/staff/Techstaff/DatabaseAdmin.java
+++ src/main/java/staff/Techstaff/DatabaseAdmin.java
@@ -9,36 +9,5 @@ public class DatabaseAdmin extends Employee {
         super(name, NI_nunber, salary);
     }
 
-        public String getName () {
-            return name;
-        }
-
-        public void setName (String name){
-            this.name = name;
-        }
-
-        public String getNI_number () {
-            return NI_number;
-        }
-
-        public void setNI_number (String NI_number){
-            this.NI_number = NI_number;
-        }
-
-        public int getSalary () {
-            return salary;
-        }
-
-        public void setSalary ( int salary){
-            this.salary = salary;
-        }
-
-        public double raiseSalary ( double raiseMultiplier){
-            return this.salary * raiseMultiplier;
-        }
-
-        public double payBonus ( double payBonusMultiplier){
-            return this.salary * payBonusMultiplier;
-        }
 
 }
